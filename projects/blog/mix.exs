046a8962fff00cc32ab3a18bcbbae55c66047c05
Chrisna Adhi Pranoto <chrisnaadhip@gmail.com>
2024-05-27 16:00:26 +0700

    add timex

--- projects/blog/mix.exs
+++ projects/blog/mix.exs
@@ -58,7 +58,8 @@ defmodule Blog.MixProject do
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:bandit, "~> 1.2"}
+      {:bandit, "~> 1.2"},
+      {:timex, "~> 3.0"}
     ]
   end
 
